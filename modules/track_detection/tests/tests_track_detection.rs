78ac08ff47f0d9ff550c27aa75e3fd1db1366fb3
Florian Weßel <florianwessel@gmx.net>
2025-11-05 22:13:47 +0100

    feat: new module for automatic track detection
    Signed-off-by: Florian Weßel <florianwessel@gmx.net>

---
+++ modules/track_detection/tests/tests_track_detection.rs
@@ -0,0 +1,75 @@
+use chrono::NaiveDate;
+use chrono::NaiveTime;
+use common::{position::GnssPosition, test_helper::track::get_track};
+use module_core::ModuleCtx;
+use module_core::{
+    Event, EventBus, EventKind, EventKindDiscriminants, GnssPositionPtr, Module, Request, Response,
+    payload_ref,
+    test_helper::{ResponseHandler, stop_module, wait_for_event},
+};
+use std::time::Duration;
+use tokio::task::JoinHandle;
+use track_detection::TrackDetection;
+
+fn create_module(ctx: ModuleCtx) -> JoinHandle<Result<(), ()>> {
+    tokio::spawn(async move {
+        let mut td = TrackDetection::new(ctx);
+        td.run().await
+    })
+}
+
+#[tokio::test]
+pub async fn handle_track_detection_request() {
+    let event_bus = EventBus::default();
+    let mut td = create_module(event_bus.context());
+
+    let _rsph = ResponseHandler::new(
+        event_bus.context(),
+        EventKindDiscriminants::LoadAllStoredTracksRequestEvent,
+        Event {
+            kind: EventKind::LoadAllStoredTracksResponseEvent(
+                Response {
+                    id: 0,
+                    receiver_addr: 20,
+                    data: vec![get_track()],
+                }
+                .into(),
+            ),
+        },
+    );
+    event_bus.publish(&Event {
+        kind: EventKind::DetectTrackRequestEvent(
+            Request {
+                id: 0,
+                sender_addr: 11,
+                data: (),
+            }
+            .into(),
+        ),
+    });
+    event_bus.publish(&Event {
+        kind: EventKind::GnssPositionEvent(GnssPositionPtr::new(GnssPosition::new(
+            52.0258333,
+            11.279166666,
+            20.0,
+            &NaiveTime::parse_from_str("00:00:00.000", "%H:%M:%S%.3f").unwrap(),
+            &NaiveDate::parse_from_str("01.01.1970", "%d.%m.%Y").unwrap(),
+        ))),
+    });
+
+    let event = wait_for_event(
+        &mut event_bus.subscribe(),
+        Duration::from_millis(100),
+        EventKindDiscriminants::DetectTrackResponseEvent,
+    )
+    .await;
+
+    let event_payload = payload_ref!(event.kind, EventKind::DetectTrackResponseEvent);
+    assert!(event_payload.is_some());
+    let event_payload = event_payload.unwrap();
+    assert_eq!(event_payload.id, 0);
+    assert_eq!(event_payload.receiver_addr, 11);
+    assert_eq!(event_payload.data, vec![get_track()]);
+
+    stop_module(&event_bus, &mut td).await
+}
